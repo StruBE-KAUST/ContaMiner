5c5535290193efc53e98dacdc267b2ed2f4621a0
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2018-09-09 17:05:25 +0300

    BUG: Print all errors to stderr.

--- contaminer/ccp4.py
+++ contaminer/ccp4.py
@@ -57,14 +57,18 @@ class Morda():
 
         popen.wait()
         if popen.returncode != 0:
-            print("Call to %s failed." % 'morda_' + self.tool, file=sys.stderr)
-            print('-' * 50)
+            print("Call to %s failed." % 'morda_' + self.tool,
+                  file=sys.stderr)
+            print('-' * 50,
+                  file=sys.stderr)
             print("Output: \n%s" % popen.stdout.read().decode('UTF-8'),
                   file=sys.stderr)
-            print('-' * 50)
+            print('-' * 50,
+                  file=sys.stderr)
             print("Error: %s" % popen.stderr.read().decode('UTF-8'),
                   file=sys.stderr)
-            print('-' * 50)
+            print('-' * 50,
+                  file=sys.stderr)
             raise RuntimeError("Call to morda_%s failed." % self.tool)
 
         self.output = popen.stdout.read()
