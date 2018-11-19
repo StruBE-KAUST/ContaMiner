d9d08bdb37c6678d81887eaaebfa6483c05fa9c2
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2018-11-19 13:16:50 +0300

    ENH: Write entrypoint to solve from saved args.

--- contaminer/contaminer.py
+++ contaminer/contaminer.py
@@ -1,6 +1,14 @@
 """Provide entry point commands for ContaMiner."""
 
-from contaminer.args_manager import ArgumentsListManager
+import sys
+import time
+from mpi4py import MPI
+
+from contaminer.args_manager import TasksManager
+from contaminer.ccp4 import MordaSolve
+
+MASTER_RANK = 0
+MRD_RESULTS_TAG = 11
 
 
 def prepare(diffraction_file, models):
@@ -17,8 +25,72 @@ def prepare(diffraction_file, models):
 
     """
 
-    arguments_manager = ArgumentsListManager()
-    arguments_manager.create(diffraction_file, models)
-    arguments_manager.save("args.json")
+    tasks_manager = TasksManager()
+    tasks_manager.create(diffraction_file, models)
+    tasks_manager.save("args.json")
 
-    print("Need %s processes." % len(arguments_manager._args_list))
+    # Number of workers + 1 master
+    print("Need %s processes."
+          % str(len(tasks_manager.get_arguments()) + 1))
+
+
+def solve(args_file):
+    """
+    Run the morda_solve processes for the given arguments file.
+
+    Parameters
+    ----------
+    args_file: string
+        Path to the arguments file generated during the prepare step.
+
+    """
+
+    mpi_comm = MPI.COMM_WORLD
+    mpi_size = mpi_comm.Get_size()
+    mpi_rank = mpi_comm.Get_rank()
+
+    args_list = None
+    # Load args on master rank.
+    if mpi_rank == MASTER_RANK:
+        tasks_manager = TasksManager()
+        tasks_manager.load(args_file)
+        args_list = tasks_manager.get_arguments()
+
+        # Add an empty space for master as it does not need args.
+        args_list.insert(MASTER_RANK, None)
+
+        if mpi_size < len(args_list):
+            print("Not enough MPI sockets", file=sys.stderr)
+            mpi_comm.Abort(1)
+
+        # Change tasks status to "running".
+        tasks_manager.update(*range(mpi_size-1), status="running")
+
+    # Send args to all ranks.
+    arguments = mpi_comm.scatter(args_list, root=MASTER_RANK)
+
+    # Compute on all worker ranks.
+    if mpi_rank != MASTER_RANK:
+        mrds = MordaSolve(**arguments)
+        mrds.run()
+        results = mrds.get_results()
+        ranked_results = {
+            'rank': mpi_rank,
+            'results': results
+        }
+        # Send results tag = MRD_RESULTS_TAG
+        mpi_comm.send(ranked_results, dest=MASTER_RANK, tag=MRD_RESULTS_TAG)
+
+    # Receive results on rank #0
+    else:
+        while not tasks_manager.complete:
+            tasks_manager.display_progress()
+
+            # Get results sent on tag = MRD_RESULTS_TAG a few lines before.
+            new_result = mpi_comm.recv(
+                source=MPI.ANY_SOURCE, tag=MRD_RESULTS_TAG)
+
+            tasks_manager.update(
+                new_result['rank'],
+                results=new_result['results'],
+                status="complete")
