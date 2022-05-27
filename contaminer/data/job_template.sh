e669fe1f4b1862f082551389b8e4f0bd5370cf76
Dunatotatos <contact@dunatotatos.com>
2022-05-27 12:37:05 +0200

    Fix job template array indexes.

--- contaminer/data/job_template.sh
+++ contaminer/data/job_template.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /home/ahungler/KAUST/ContaMiner
-source venv/bin/activate
-source /home/ahungler/ccp4/ccp4-7.1/bin/ccp4.setup-sh
-seq 1 %NB_PROCS% | xargs -n 1 -P 12 -I {} contaminer %COMMAND% {}
+. venv/bin/activate
+. /home/ahungler/KAUST/ccp4-7.1/bin/ccp4.setup-sh
+. /home/ahungler/KAUST/MoRDa_DB/morda_env_sh
+seq %MIN_ARRAY% %MAX_ARRAY% | xargs -n 1 -P 12 -I {} contaminer %COMMAND% {}
