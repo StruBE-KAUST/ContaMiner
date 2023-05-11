10d1edeb83c3e6944dce18b624bcc0c4ba151e01
Dunatotatos <contact@dunatotatos.com>
2023-05-11 16:23:47 +0200

    Remove sourcing of CCP4 and MoRDa in job templates since ContaMiner does the job automatically.

--- contaminer/data/job_template.sh
+++ contaminer/data/job_template.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-cd /home/ahungler/KAUST/ContaMiner
-. venv/bin/activate
-. /home/ahungler/KAUST/ccp4-7.1/bin/ccp4.setup-sh
-. /home/ahungler/KAUST/MoRDa_DB/morda_env_sh
 seq %MIN_ARRAY% %MAX_ARRAY% | xargs -n 1 -P 12 -I {} contaminer %COMMAND% {}
